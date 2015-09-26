3722c3cbab06ed3f6e49088bb06df86ca7dc0ac3
Sorosliu1029 <soros.liu1029@gmail.com>
2015-09-26 19:26:35 +0800

    Finish exercise 2 main part

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,10 +19,10 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h_theta = 1 ./ (1 + exp(-1 * (X * theta)));
+J = -1 / m * (y' * log(h_theta) + (1 - y)' * log(1 - h_theta));
 
-
-
-
+grad = 1 / m * ((h_theta - y)' * X)';
 
 
 
